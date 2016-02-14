9cc74b5ced31f24fa452a66ea92ece3f90faa7f3
andy fang <andy@mingtak.com.tw>
2016-02-14 02:39:36 +0000

    add delete old contents function

--- twgov/content/browser/delete_old_contents.py
+++ twgov/content/browser/delete_old_contents.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
-#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-#from plone import api
-#from plone.memoize import ram
-#from time import time
 import logging
 from DateTime import DateTime
-#import transaction
 
 
 class DeleteOldContents(BrowserView):
-
-#    template = ViewPageTemplateFile('templates/delete_old_contents.pt')
 
     def __call__(self):
 
@@ -39,14 +32,9 @@ class DeleteOldContents(BrowserView):
             count += 1
             objectList.append(item.id)
 
-#            logger.info('(%s/%s) Delete Content id: %s' % (count, lenBrain, item.id))
-#            api.content.delete(obj=item.getObject())
             if count // (int(request['count']) if request.has_key('count') else 1000):
-#                logger.info('Finish! End Delete Old Contents.')
                 logger.info('Creat List OK.')
                 break
-#                transaction.commit()
-#        transaction.commit()
         folder = brain[0].getObject().aq_parent
         folder.manage_delObjects(objectList)
         logger.info('Finish! End Delete Old Contents.')
